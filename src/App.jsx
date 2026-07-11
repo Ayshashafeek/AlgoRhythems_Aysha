14ec88f8f9c0136a1b578ff6bd4d3b9e8ff95c9d
PournamyPS <pournamyps9c@gmail.com>
2026-07-12 01:11:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import TrainingStudio from "./pages/TrainingStudio.jsx";
-
+import ChoreographyStudio from "./pages/ChoreographyStudio.jsx";
+// ...
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/training" element={<TrainingStudio />} />
+      <Route path="/choreography" element={<ChoreographyStudio />} />
     </Routes>
   );
 }
